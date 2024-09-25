81a811c2075f6cec069d7684168e3257e11953e5
Klnishant <nishantkaushal075@gmail.com>
2024-09-25 14:13:17 +0530

    Create views for tweet_list

--- tweet/views.py
+++ tweet/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Tweet
+from .forms import TweetForm, UserRegisterForm
+from django.shortcuts import redirect, get_object_or_404
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def tweet_list(request):
+    tweets = Tweet.objects.all().order_by('-created_at')
+    return render(request,'tweet_list.html',{'tweets':tweets})
