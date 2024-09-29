055af524aa26bfd900bb8dd0684eb14f4a26d42b
Klnishant <nishantkaushal075@gmail.com>
2024-09-29 13:10:43 +0530

    Add navbar from bootstrap in layout.html

--- tweet/urls.py
+++ tweet/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('',views.tweet_list,name='tweet_list'),
+    path('create/',views.CreateTweet,name='tweet_create'),
+    path('<int:tweet_id>/edit/',views.EditTweet,name='tweet_edit'),
+    path('<int:tweet_id>/delete/',views.DeleteTweet,name='tweet_delete'),
+    path('register/',views.register,name='register')
 ]
